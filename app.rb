b762758da16be0825a4a286952d99db399e1513a
fridayshoes <one_amp@hotmail.com>
2022-05-31 16:44:29 +0100

    Walkthrough – Entering Players

--- app.rb
+++ app.rb
@@ -4,12 +4,24 @@ require 'sinatra/reloader'
 class Battle < Sinatra::Base
   configure :development do 
     register Sinatra::Reloader
+  
+    get '/' do
+      'Testing infrastructure working!'
+    end
+    
+    get '/' do
+      erb :index
+    end
+    
+    post '/names' do
+      @player_1_name = params[:player_1_name]
+      @player_2_name = params[:player_2_name]
+      erb :play
+    end
+  
   end
-
-  get '/' do
-    'Testing infrastructure working!'
-  end
-
   run! if app_file ==$0
-end 
+end
+
+
 
