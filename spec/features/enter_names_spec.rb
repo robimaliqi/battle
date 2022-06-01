02598d666c1ded93e29f3561c49f5e5f178d0a77
Robi <robi.maliqi@hotmail.co.uk>
2022-06-01 16:58:08 +0100

    Test Helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,9 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
 
     #save_and_open_page # will save the web page and open the browser to display it
-    
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
