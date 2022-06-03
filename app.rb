f1b89f4c21cc0069e3e1a899a9aba177cc90d85a
Robi <robi.maliqi@hotmail.co.uk>
2022-06-03 15:08:37 +0100

    Single Responsibility Principle

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require './lib/player.rb'
+require './lib/game.rb'
 
 class Battle < Sinatra::Base
   configure :development do 
@@ -35,7 +36,7 @@ class Battle < Sinatra::Base
     get '/attack' do
       @player_1 = $player_1
       @player_2 = $player_2
-      @player_1.attack(@player_2)
+      Game.new.attack(@player_2)
       erb :attack
     end
 
